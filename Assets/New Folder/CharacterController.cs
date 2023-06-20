2f65dbfa0b1bf71129c4f91d23280a3c94b84fcc
chourouk07 <117457141+chourouk07@users.noreply.github.com>
2023-06-20 03:59:13 +0100

    Commit

--- Assets/New Folder/CharacterController.cs
+++ Assets/New Folder/CharacterController.cs
@@ -5,9 +5,24 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     [SerializeField] private Animator _animator;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private Transform _groundCheck;
+
+    [SerializeField] private bool _isGrounded;
+    public float _jumpHeight= 5f;
+
+    
+    private Rigidbody2D _rb;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, 0.1f, _groundLayer);
         float _horizontalInput = Input.GetAxis("Horizontal");
         if (_horizontalInput != 0)
         {
@@ -15,15 +30,27 @@ public class CharacterController : MonoBehaviour
             if (_horizontalInput > 0)
             {
                 transform.Translate(Vector2.right * _speed * Time.deltaTime);
+                transform.localScale = new Vector2(1.5f, 1.5f);
             }
             if (_horizontalInput < 0)
             {
                 transform.Translate(Vector2.left * _speed * Time.deltaTime);
+                transform.localScale = new Vector2(-1.5f,1.5f);
             }
         }
         else
         {
             _animator.SetBool("Running", false);
         }
+
+        if (_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _animator.SetTrigger("Jumping");
+        }
+    }
+
+    public void OnJump() 
+    {
+        _rb.AddForce(new Vector2(0f, _jumpHeight), ForceMode2D.Impulse);
     }
 }
